cd9eafa8868808c0b09ba5d33b15ff5f3230ea4e
doljae <seok9211@naver.com>
2022-04-29 17:51:25 +0900

    item2, sequence

--- src/main/kotlin/item2.kt
+++ src/main/kotlin/item2.kt
@@ -10,3 +10,29 @@ fun updateWeather(degree: Int) {
 enum class Color {
     RED, BLUE, YEllOW
 }
+
+fun main() {
+
+    // v1
+//    var numbers = (2..100).toList()
+//    val primes = mutableListOf<Int>()
+//    while (numbers.isNotEmpty()) {
+//        val prime = numbers.first()
+//        primes.add(prime)
+//        numbers = numbers.filter { it % prime != 0 }
+//    }
+
+    //v2
+    val primes: Sequence<Int> = sequence {
+        var numbers = generateSequence(2) { it + 1 }
+        while (true) {
+            val prime = numbers.first()
+            yield(prime)
+            numbers = numbers.drop(1).filter { it % prime != 0 }
+        }
+    }
+
+
+    print(primes.take(10).toList())
+
+}
